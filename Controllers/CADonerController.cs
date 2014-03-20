1d728e7d4eda49b6f121712b2a6258ba13be9763
JacobBower <bower.jacob@gmail.com>
2014-03-20 12:10:53 -0700

    A MVC project for a final project in asp.NET

---
+++ Controllers/CADonerController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcFinal.Models;
+
+namespace MvcFinal.Controllers
+{
+    public class CADonerController : Controller
+    {
+        //
+        // GET: /CAEmployee/
+
+        public ActionResult Index()
+        {
+            CommunityAssistEntities cad = new CommunityAssistEntities();
+            var doners = from d in cad.Donations
+                            orderby d.Person.PersonLastName
+                            select new
+                            {
+                                d.Person.PersonLastName,
+                                d.Person.PersonFirstName,
+                                d.Person.Donations,
+                                d.DonationAmount,
+                                d.Person.PersonUsername
+
+                            };
+            List<CADoner> donerList = new List<CADoner>();
+            foreach (var x in doners)
+            {
+                CADoner don = new CADoner();
+                don.LastName = x.PersonLastName;
+                don.FirstName = x.PersonFirstName;
+                don.Email = x.PersonUsername;
+                don.Donation = x.DonationAmount.ToString();
+
+                donerList.Add(don);
+            }
+
+            return View(donerList);
+        }
+
+    }
+
+    public class CADoner
+    {
+        public string LastName { set; get; }
+        public string FirstName { set; get; }
+        public string Email { set; get; }
+        public string Donation { set; get; }
+    }
+}
